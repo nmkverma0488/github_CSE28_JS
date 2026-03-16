3f800285e14eebe76555a74d62b9404dd2f288c0
Nirjara Verma <nirjarav5710@gmail.com>
2026-03-16 15:05:07 +0530

    commit

--- new.js
+++ new.js
@@ -1,4 +1,12 @@
-const{log}=require("console");
+/*const{log}=require("console");
 console.log("hello world");
 var a=20;
-console.log("a");
+console.log(a);
+var b=20;
+var c=a+b;
+console.log(c);88*/
+console.log("let introduction");
+let a=10;
+a=90;
+let a=30;
+console.log(a);
