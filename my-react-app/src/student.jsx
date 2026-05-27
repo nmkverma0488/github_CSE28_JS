5d6482ff7d1b6b1af13066acb2dbfeb3d58dae63
Nirjara Verma <nirjarav5710@gmail.com>
2026-05-27 09:35:03 +0530

    commit

---
+++ my-react-app/src/student.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Student(props) {
+  return (
+    <div>
+      <h2>Name:{props.name}</h2>
+      <p>Course:{props.course}</p>
+      <p>Marks:{props.marks}</p>
+      {/*
+        <h1>Hello</h1>
+    <h2>Student Information</h2>
+    
+      <audio controls>
+        <source src="" type="audio/mp3/"></source>
+      </audio>
+      <video controls>
+        <source src="" type="video/mp4/"></source>
+      </video>
+      <iframe src="https://www.youtube.com/embed/FpM9m12qjZE"></iframe>
+      <img src="https://images.pexels.com/photos/35383830/pexels-photo-35383830.jpeg" width="500" height="500"></img><br></br>
+      <button>Click ME</button>
+    <h2>Hello ABESEC</h2>
+    <p>Hello Raj</p>
+      */}
+
+    </div>
+  )
+}
+
+export default Student
